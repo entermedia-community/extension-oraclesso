452b594c7219f5cbddd11f157d0b7821a7676ed6
david <david@entermediasoftware.com>
2011-07-08 14:42:02 -0400

    now auto creates user and groups

--- webapp/oraclesso/sso.groovy
+++ webapp/oraclesso/sso.groovy
@@ -1,27 +1,48 @@
-//check for a value in the header
-//use that value to request information from from a url should return json
-//parse the returned json
+import java.util.Iterator
 
 import javax.servlet.http.HttpServletRequest
 
 import com.google.gson.Gson
 import com.openedit.modules.update.Downloader
+import com.openedit.users.Group
 import com.openedit.users.User
 import com.openedit.users.UserManager
 
-public class Person
+public class Team
 {
-	public String email;
+	public String id;
+	public String location;
+	public String publicName;
+	public String prettyPublicName;
 }
 
-protected User createNewUser(Person inPerson)
+public class Status
 {
-	//create a user object
-	//go through a list of teams they are on
-	//look up each team
-	//create team if it doesn't exist
-	//add the user to the team/group
-	return null;
+	public String desc;
+	public String ok;
+}
+
+public class TeamInfo
+{
+	public List<Team> teams;
+	public Status status;
+}
+
+//go through list of teams in teaminfo
+protected void generateTeams(User inUser, TeamInfo inTeamInfo)
+{
+	for (Iterator iterator = inTeamInfo.teams.iterator(); iterator.hasNext();) {
+		Team team = (Team) iterator.next();
+		//look up the group by name
+		UserManager um = userManager;
+		Group group = um.getGroup(team.id);
+		if(group == null)
+		{
+			group = um.createGroup(team.id, team.publicName);
+		}
+		inUser.addGroup(group);
+		um.saveUser(inUser);
+	}
 }
 
 protected String getContent(String inUrl)
@@ -34,22 +55,17 @@ protected void oracleSsoLogin()
 {
 	HttpServletRequest requestHeader = context.getRequest();
 	String pid = requestHeader.getHeader("HBS_PERSON_ID");
-	context.putSessionValue("personid", pid);
+	pid = "641929";
+	pid = "615538";
+	context.putPageValue("personid", pid);
 	//String tid = requestHeader.getHeader("teamid");  //could be multiple teamids, guess need to split these
-	//String teaminfo = http://pine-stage.hbs.edu/teamMgmt/internal/ws.htm?action=getTeamInfo&teamId=498211;
-	String personinfo = "http://pine-stage.hbs.edu/teamMgmt/internal/ws.htm?action=getTeamInfo&personId=615538";
+	//String teaminfo = "http://pine-stage.hbs.edu/teamMgmt/internal/ws.htm?action=getTeamInfo&teamId=498211";
+	String personinfo = "http://pine-stage.hbs.edu/teamMgmt/internal/ws.htm?action=getTeamInfo&personId=";
 	
 	String jsonteaminfo;
 	try
 	{
-		if(pid)
-		{
-			jsonteaminfo = getContent(personinfo + pid);
-		}
-		else
-		{
-			jsonteaminfo = getContent(personinfo);
-		}
+		jsonteaminfo = getContent(personinfo + pid);
 	}
 	catch(Exception e)
 	{
@@ -58,19 +74,24 @@ protected void oracleSsoLogin()
 	
 	context.putPageValue("jsonresponse", jsonteaminfo);
 	
-	//Gson gson = new Gson();
-	//Person person = gson.fromJson(jsonteaminfo, Person.class);
-	
+	Gson gson = new Gson();
+	TeamInfo teams = gson.fromJson(jsonteaminfo, TeamInfo.class);
+		
 	//search for a user
-	//UserManager um = userManager;
-	//User user = um.getUserByEmail(person.email);
-	//if(user == null)
-	//{
-	//	user = createNewUser(person);	
-	//}
+	UserManager um = userManager;
+	User user = um.getUser(pid);
+	if(user == null)
+	{
+		user = um.createUser(pid, null);
+		Group guestgroup = um.getGroup("guest");
+		user.addGroup(guestgroup);
+		um.saveUser(user);
+	}
+	
+	generateTeams(user, teams);
 	
 	//auto login user
-	//context.putSessionValue("user", user);
+	context.putSessionValue("user", user);
 }
 
 oracleSsoLogin();
