2efdb854f7ced9257341d96646227bbd5c25d026
david <david@entermediasoftware.com>
2011-07-08 09:31:42 -0400

    tested, does pull down sso data

--- webapp/oraclesso/sso.groovy
+++ webapp/oraclesso/sso.groovy
@@ -14,13 +14,21 @@ protected String getContent(String inUrl)
 
 protected void oracleSsoLogin()
 {
-	HttpServletRequest requestHeader = context.getHeader();
+	HttpServletRequest requestHeader = context.getRequest();
 	String pid = requestHeader.getHeader("HBS_PERSON_ID");
 	//String tid = requestHeader.getHeader("teamid");  //could be multiple teamids, guess need to split these
-	String personinfo = "http://rana1-stage.hbs.edu:8097/teamMgmt/external/ws.htm?action=getTeamInfo&personId=";
-	//String teaminfo = "http://rana1-stage.hbs.edu:8097/teamMgmt/external/ws.htm?action=getTeamInfo&teamId=";
+	//String teaminfo = http://pine-stage.hbs.edu/teamMgmt/internal/ws.htm?action=getTeamInfo&teamId=498211;
+	String personinfo = "http://pine-stage.hbs.edu/teamMgmt/internal/ws.htm?action=getTeamInfo&personId=615538";
 	
-	String jsonteaminfo = getContent(personinfo + pid);
+	String jsonteaminfo;
+	if(pid)
+	{
+		jsonteaminfo = getContent(personinfo + pid);
+	}
+	else
+	{
+		jsonteaminfo = getContent(personinfo);
+	}
 	//String jsonteam = getContent(teaminfo + tid);
 	context.putPageValue("jsonresponse", jsonteaminfo);
 }
