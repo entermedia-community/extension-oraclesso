1bac0ca8097c14c7b3a251ecaa927e77fe8f8411
Don Livanec <don@openedit.org>
2011-07-14 14:02:07 -0400

    finished hbu soap call, java called by sso.groovy

---
+++ test/com/entermedia/soap/RolesTest.java
@@ -0,0 +1,24 @@
+package com.entermedia.soap;
+
+import org.junit.Test;
+import org.openedit.entermedia.BaseEnterMediaTest;
+
+import com.openedit.WebPageRequest;
+
+public class RolesTest extends BaseEnterMediaTest {
+
+	@Test
+	public void testAddRoles() 
+	{
+		WebPageRequest req = getFixture().createPageRequest("/hbs/authentication/nopermissions.html");
+		req.setRequestParameter("HBS_PERSON_ID","641292");
+		getFixture().getEngine().executePathActions(req);
+		
+	}
+
+//	@Test
+//	public void XtestAddUserData() {
+//		fail("Not yet implemented");
+//	}
+
+}
